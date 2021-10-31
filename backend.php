30bfc6ed0e075729fd5efce38c6aed585b5284f7
aarush1267 <80965392+aarush1267@users.noreply.github.com>
2021-10-31 15:13:39 +0530

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,11 +1,29 @@
 <?php
 
+// Making Connection To The Database
+
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPass = "";
+$database = "signup";
+
+$connection = mysqli_connect($dbHost, $dbUser, $dbPass, $database) or die ("Sorry, we could not connect to the database");
+
+// Login System
+
 if(isset($_POST['uname']) && isset($_POST['psw'])) {
   $uname = $_POST['uname'];
   $psw = $_POST['psw'];
-  echo $uname;
-} else {
-  echo "Not working.";
+
+  $process = "SELECT * FROM users WHERE email='$uname' AND password='$psw'";
+  $res = mysqli_query($connection, $process);
+
+  if (mysqli_num_rows($res) == 1) {
+    echo "Successful login!";
+  } else {
+    echo "Incorrect Email/Password";
+  }
+
 }
 
 ?>
